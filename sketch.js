c58c7ce0d0f00e3c7fb59d3f11f98ac8b408b9d1
felixcaso <54224139+felixcaso@users.noreply.github.com>
2020-08-02 17:58:43 -0400

    8/2/2020
    updated player hit box
    updated restart with space bar

--- sketch.js
+++ sketch.js
@@ -1,113 +1,167 @@
+//Game variables
 var startGame = true;
 var score;
-var velocity = .7;
-var noteCounter;
+var noteCounter=1;
+var notes; //Sprite Group
+var player; //Player Object
+var enemy; //Enemy Object
 
+//Borders to falling line with backGround
 var leftWall = 370; 
 var rightWall = 1010;
 
+//Images Variables
 var bgImg;
 var cleftImg;
 var noteImg = [];
 var playerImg;
 
-var notes;
-var player;
-var enemy;
-
+//Fonts
+var OPEN_SAN_LIGHT;
+var PLAY_PAIR_DISPLAY_BOLD;
 
 //Preloading Images
 function preload(){
+    //Images
     for(var i=0; i<3;i++){
         noteImg[i] = loadImage('Images/musicNote'+i+'.png');
     }  
     bgImg = loadImage('Images/bg2.jpg');
     cleftImg = loadImage('Images/clef.png')
     playerImg = loadImage('Images/player.png');
+
+    //Fonts
+    OPEN_SANS_LIGHT = loadFont('Fonts/OpenSans-Light.ttf');
+    PLAY_FAIR_DISPLAY_BOLD = loadFont('Fonts/PlayfairDisplay-Bold.ttf');
 }
 
 function setup() {
     //Create Canvas
     createCanvas(windowWidth,windowHeight);
-    //cursor('NONE');
+    cursor('NONE');
     
-    //Inital Droping note
+    //Group of Dropping note
     notes = new Group();
-    
-    var note = createSprite(random(leftWall,rightWall),0,10,10);
-    note.scale = random(.1,.2);
-    note.setVelocity(0,random(3,10));
-    note.debug = true;
-    note.addImage(random(noteImg));
-    notes.add(note);
-//    
-    //Creating enemy sprite
-    enemy = createSprite(random(leftWall,rightWall),0,10,10);
-    enemy.scale = random(.1,.4);
-    enemy.setVelocity(0,random(3,10));
-    enemy.debug=true;
-    enemy.addImage(cleftImg);
-    notes.add(enemy);    
+
+    //inital note
+    newNote(noteCounter);
+
+    // Enemy Sprite
+    newEnemy();
     
     //Creating Player sprite 
-    player = new Player(mouseX,windowHeight-100,10,10,playerImg);    
-        
+    player = new Player(mouseX,windowHeight-100,10,10,playerImg);
     startGame = true;
     score = 0;
-    newNoteCounter = 0;
 }//end setup
+
+function setText(){
+    //Instructions
+    strokeWeight(0);
+    textSize(16);
+    textStyle(BOLD);
+    textFont(PLAY_FAIR_DISPLAY_BOLD);
+
+    //fill(235,81,15); //OBF color (red-oraange)
+    fill(0);
+    text("OBF-Dodge The Trebel-Clef ", 830,15);
+    text('Last As Long As You Can!',830,40);
+    fill(150);
+    //text('('+mouseX+','+mouseY+')', 900,300);
+
+    // Iymanni's Logo
+    fill(255);
+    ellipse(1300,55,110);
+    fill(0);
+    textSize(50)
+    textStyle(NORMAL);
+    text("iah",1265,70);
+
+    //Display Score
+    textSize(50);
+    textAlign(LEFT,TOP);
+    fill(235,81,15);
+    text('Score: '+score,10,35);
+}
 
 function draw() {
     if(startGame){
-        //background(150); 
-        background(bgImg)
-        textSize(50);
-        textAlign(LEFT,TOP);
-        fill(255,0,0);
-        text('Score: '+score,10,10);
+        //Background
+        background(bgImg);
+        setText(); // Settiing Iymanni's Logo && Instructions && Score
 
-        drawSprites();
-        player.move();
-
+        //Increase score every 60s and drop new note
         if(frameCount %20 == 0){
-            score++;             
+            score++;
+            noteCounter++;
+            newNote(noteCounter);
         }
 
+        //Sprite  movement
         for(var i=0; i<allSprites.length; i++) {
-            var s = allSprites[i];        
-            if(s.position.y>height+10) {            
-                s.position.y = 0;
+            var s = allSprites[i];
+            if(s.position.y> height+10) {
+                s.position.y = random(-200,-100);
                 s.scale = random(.2,.4);
                 s.position.x = random(leftWall,rightWall);
                 s.setVelocity(0,random(7,20));
             }
-        }    
+        }
+        //Draw sprites
+        drawSprites();
+        player.move();
     }
     
 }
 
 function keyPressed(){
-    
+    if(!startGame){
+        noteCounter = 1;
+        newNote(noteCounter)
+        newEnemy();
+        startGame = true;
+    }
 }
 
 function lose(){
-    background(255,0,0);    
+    //Losing display
+    background(235,81,15);
+    startGame = false;
+    notes.removeSprites();
+
     textSize(50);
-    fill(0,255,0);
-    text('Score: '+score,10,10);
-    startgame = false;
-    noloop();
+    fill(0);
+    text('Score: '+score,450,300);
+    text('Press any Key to restart',450,400);
+
+    // Iymanni's Logo
+    fill(255);
+    ellipse(1300,55,110);
+    fill(0);
+    textSize(50)
+    textStyle(NORMAL);
+    text("iah",1265,20);
 }
 
-function newNote(){
-    var newNote = createSprite(random(leftWall,rightWall),-100,10,10);        
-    newNote.scale = random(.2,.3);
-    newNote.setVelocity(0,random(3,10));
-    newNote.debug = true;
-    newNote.addImage(random(noteImg));
-    notes.add(newNote);
-    
-    //noteCounter++;
+function newNote(n){
+    if(n<7){
+        var note = createSprite(random(leftWall,rightWall),random(-300,-100),50,50);
+        note.scale = random(.1,.3);
+        note.setVelocity(0,random(3,10));
+        note.debug = true;
+        note.addImage(random(noteImg));
+        notes.add(note);
+    }
+
+}
+
+function newEnemy(){
+    enemy = createSprite(random(leftWall,rightWall),random(-300,-100),10,10);
+    enemy.scale = random(.1,.4);
+    enemy.setVelocity(0,random(3,10));
+    enemy.debug=true;
+    enemy.addImage(cleftImg);
+    notes.add(enemy);
 }
 
 class Player{
@@ -123,7 +177,7 @@ class Player{
         
         this.playerSprite.position.x = constrain(mouseX,370,1010);
         this.playerSprite.collide(enemy,lose);
-//       
+//
     }
 }
 
